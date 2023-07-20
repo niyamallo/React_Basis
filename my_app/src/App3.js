bda24c353cc5e7826357ab569d53beb21d75b480
niyamallo <treenicole@naver.com>
2023-07-20 23:56:39 +0900

    20230720 ToDoList

---
+++ my_app/src/App3.js
@@ -0,0 +1,38 @@
+import { useState, useEffect } from "react";
+
+function App3 () {
+    const [toDo, setToDo] = useState("");
+    const [toDos, setToDos] = useState([]);
+    const onChange = (event) => setToDo(event.target.value);
+    const onSubmit = (event) => {
+        event.preventDefault();
+        if (toDo === "") {
+            return; // 비어있으면 함수가 작동되지 않도록.
+        }
+        setToDos(currentArray => [toDo, ...currentArray]);
+        setToDo("");
+        // ...를 붙이면 array에서 element들이 빠져나옴.
+        // ...를 붙이지 않는다면 toDo와 기존의 Array가 들어있는 이중의 Array가 만들어질 것
+    };
+
+    return (
+        <div>
+            <h1>My To Dos ({toDos.length})</h1>
+            <form onSubmit={onSubmit}>
+                <input
+                    onChange={onChange}
+                    value={toDo}
+                    type="Text"
+                    placeholder="Write your to do..."
+                />
+                <button>
+                    Add To Do
+                </button>
+            </form>
+            <hr />
+            {toDos.map((item, index) => <li key={index}>{item}</li>)}
+        </div>
+    );
+}
+
+export default App3;
--- my_app/src/index.js
+++ my_app/src/index.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App';
-import App2 from './App2';
+// import App from './App';
+// import App2 from './App2';
+import App3 from './App3';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
@@ -9,7 +10,8 @@ root.render(
     // 컴포넌트가 여러 엘리먼트를 return 할때 jsx규칙상 하나의 태그로 묶어서 return 해줘야 한다.
     // 이때 fragment를 사용하면 dom에 별도의 노드를 추가하지 않고 여러자식을 그룹화 할 수 있다.
     <React.Fragment>
-        <App />
-        <App2 />
+        {/* <App />
+        <App2 /> */}
+        <App3 />
     </React.Fragment>
 );
